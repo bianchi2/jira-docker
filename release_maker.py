2f1e70ca4f5fff94c76a38c3f92573d609d514f1
Yevhen Ivantsov <yivantsov@atlassian.com>
2023-02-27 10:32:41 +1100

    Test

--- release_maker.py
+++ release_maker.py
@@ -1,23 +1,24 @@
 import docker
+import os
 
 platforms = ['linux/arm64', 'linux/amd64']
 repo = 'eivantsov/jira'
 tag = '9.4.0'
 client = docker.from_env()
 
-for platform in platforms:
-    print(f'Building Jira for {platform} architecture')
-    build = client.api.build(quiet=False, buildargs={'JIRA_VERSION': tag}, path='.', platform=platform,
-                             tag=repo+':'+tag)
+ARCH = os.getenv('ARCH')
+print(f'Building Jira for {ARCH} architecture')
+build = client.api.build(quiet=False, buildargs={'JIRA_VERSION': tag}, path='.', platform=ARCH,
+                         tag=repo+':'+tag)
 
-    for line in build:
-        print(line)
+for line in build:
+    print(line)
 
-    push = client.api.push(
-        repo+':'+tag,
-        stream=True,
-        decode=True,
-    )
+push = client.api.push(
+    repo+':'+tag,
+    stream=True,
+    decode=True,
+)
 
-    for line in push:
-        print(line)
+for line in push:
+    print(line)

fe0e1754dc4fbc9d94768fb69129ad5790b6c812
Yevhen Ivantsov <yivantsov@atlassian.com>
2023-02-27 10:54:17 +1100

    Test

--- release_maker.py
+++ release_maker.py
@@ -7,6 +7,9 @@ tag = '9.4.0'
 client = docker.from_env()
 
 ARCH = os.getenv('ARCH')
+PASSWORD = os.getenv('DOCKER_PASSWORD')
+
+auth_config = {'username': 'eivantsov', 'password': PASSWORD}
 print(f'Building Jira for {ARCH} architecture')
 build = client.api.build(quiet=False, buildargs={'JIRA_VERSION': tag}, path='.', platform=ARCH,
                          tag=repo+':'+tag)
@@ -18,6 +21,7 @@ push = client.api.push(
     repo+':'+tag,
     stream=True,
     decode=True,
+    auth_config=auth_config
 )
 
 for line in push:

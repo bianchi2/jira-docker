f9a1d3d161d785a298e5371b38957d1cdd13b657
Yevhen Ivantsov <yivantsov@atlassian.com>
2023-02-27 11:47:04 +1100

    Use a different lib

--- release_maker.py
+++ release_maker.py
@@ -1,28 +1,20 @@
-import docker
+# import docker
+from python_on_whales import DockerClient
 import os
+
+docker = DockerClient()
 
 platforms = ['linux/arm64', 'linux/amd64']
 repo = 'eivantsov/jira'
-tag = '9.4.0'
-client = docker.from_env()
+tags = ['9.4.0']
 
 ARCH = os.getenv('ARCH')
 PASSWORD = os.getenv('DOCKER_PASSWORD')
+print(PASSWORD)
+docker.login(username='eivantsov', password=PASSWORD)
 
-auth_config = {'username': 'eivantsov', 'password': PASSWORD}
-print(f'Building Jira for {ARCH} architecture')
-build = client.api.build(quiet=False, buildargs={'JIRA_VERSION': tag}, path='.', platform=ARCH,
-                         tag=repo+':'+tag)
+build = docker.buildx.build(build_args={'JIRA_VERSION': tags[0]}, context_path='.', platforms=platforms,
+                            tags=tags, stream_logs=True, push=True)
 
 for line in build:
-    print(line)
-
-push = client.api.push(
-    repo+':'+tag,
-    stream=True,
-    decode=True,
-    auth_config=auth_config
-)
-
-for line in push:
     print(line)
